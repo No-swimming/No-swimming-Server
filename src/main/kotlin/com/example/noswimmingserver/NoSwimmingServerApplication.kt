d2a88ec2e6501709dead913b3a6c4637c4d7737b
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-11 12:24:43 +0900

    setting :: 프로젝트 세팅

---
+++ src/main/kotlin/com/example/noswimmingserver/NoSwimmingServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.noswimmingserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NoSwimmingServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<NoSwimmingServerApplication>(*args)
+}

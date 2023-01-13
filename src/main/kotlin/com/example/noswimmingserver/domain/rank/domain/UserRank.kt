695f1fcd95954ba60aff064fefac284c785c56ca
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-13 19:43:35 +0900

    add :: UserRank 엔티티 생성

---
+++ src/main/kotlin/com/example/noswimmingserver/domain/rank/domain/UserRank.kt
@@ -0,0 +1,30 @@
+package com.example.noswimmingserver.domain.rank.domain
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+import javax.validation.constraints.Max
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_user_rank")
+class UserRank(
+    @Id
+    val userId: Long,
+
+    @field:NotNull
+    @field:Max(3)
+    val grade: Int,
+
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(5)")
+    val name: String,
+
+    rank: Int,
+) {
+
+    @field:NotNull
+    var rank = rank
+        protected set
+}

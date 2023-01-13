fd64e699b30e24e60a4f04a536a5836e63a30b3e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-13 15:19:35 +0900

    add :: User 엔티티 생성

---
+++ src/main/kotlin/com/example/noswimmingserver/domain/common_user/domain/User.kt
@@ -0,0 +1,33 @@
+package com.example.noswimmingserver.domain.common_user.domain
+
+import com.example.noswimmingserver.global.enum.Authority
+import javax.persistence.*
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_user")
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(30)", unique = true)
+    val email: String,
+
+    @Column(columnDefinition = "CHAR(60)")
+    val password: String?,
+
+    @field:NotNull
+    @Column(columnDefinition = "VARCHAR(7)")
+    @Enumerated(EnumType.STRING)
+    val authority: Authority,
+
+    name: String?,
+) {
+
+    @Column(columnDefinition = "VARCHAR(5)")
+    var name: String? = name
+        protected set
+
+}

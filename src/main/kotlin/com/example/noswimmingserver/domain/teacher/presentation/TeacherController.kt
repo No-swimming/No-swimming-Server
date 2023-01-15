ceef0dcf6dc848dd13fffec7d652289e1eae431c
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-15 18:32:49 +0900

    feat :: 선생님 정보 수정 API

--- src/main/kotlin/com/example/noswimmingserver/domain/teacher/presentation/TeacherController.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/teacher/presentation/TeacherController.kt
@@ -1,22 +1,29 @@
 package com.example.noswimmingserver.domain.teacher.presentation
 
-import com.example.noswimmingserver.domain.teacher.presentation.dto.response.AccountResponse
+import com.example.noswimmingserver.domain.teacher.presentation.dto.request.UpdateTeacherInfoRequest
+import com.example.noswimmingserver.domain.teacher.presentation.dto.response.TeacherAccountResponse
 import com.example.noswimmingserver.domain.teacher.service.CreateRandomAccountService
+import com.example.noswimmingserver.domain.teacher.service.UpdateTeacherInfoService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RequestMapping("/teacher")
 @RestController
 class TeacherController(
     private val createRandomAccountService: CreateRandomAccountService,
+    private val updateTeacherInfoService: UpdateTeacherInfoService,
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    fun createRandomAccount(): AccountResponse {
+    fun createRandomAccount(): TeacherAccountResponse {
         return createRandomAccountService.execute()
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping
+    fun editTeacherInfo(@RequestBody @Valid request: UpdateTeacherInfoRequest) {
+        updateTeacherInfoService.execute(request)
     }
 }
---
+++ src/main/kotlin/com/example/noswimmingserver/domain/teacher/service/UpdateTeacherInfoService.kt
@@ -0,0 +1,25 @@
+package com.example.noswimmingserver.domain.teacher.service
+
+import com.example.noswimmingserver.domain.teacher.facade.TeacherFacade
+import com.example.noswimmingserver.domain.teacher.presentation.dto.request.UpdateTeacherInfoRequest
+import com.example.noswimmingserver.global.security.SecurityFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UpdateTeacherInfoService(
+    private val securityFacade: SecurityFacade,
+    private val teacherFacade: TeacherFacade,
+) {
+
+    @Transactional
+    fun execute(request: UpdateTeacherInfoRequest) {
+        val user = securityFacade.getCurrentUser()
+
+        val teacher = teacherFacade.getTeacherById(user.id)
+
+        user.editName(request.name)
+
+        teacher.editSubject(request.subject)
+    }
+}

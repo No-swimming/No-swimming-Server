dcecbaf66d03a719a0ca0dc0d64a914fc33d81a7
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-15 16:33:55 +0900

    add :: AuthorityUtil 추가

---
+++ src/main/kotlin/com/example/noswimmingserver/global/util/AuthorityUtil.kt
@@ -0,0 +1,9 @@
+package com.example.noswimmingserver.global.util
+
+import com.example.noswimmingserver.global.enum.Authority
+
+object AuthorityUtil {
+
+    val STUDENT = Authority.STUDENT.name
+    val TEACHER = Authority.TEACHER.name
+}

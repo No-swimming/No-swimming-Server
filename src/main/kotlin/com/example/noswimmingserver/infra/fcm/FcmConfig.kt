d490010e0972a5ff43324b27b21161ba1a8846c0
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-16 16:57:25 +0900

    add :: FcmConfig 추가

---
+++ src/main/kotlin/com/example/noswimmingserver/infra/fcm/FcmConfig.kt
@@ -0,0 +1,38 @@
+package com.example.noswimmingserver.infra.fcm
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
+import java.io.IOException
+import java.nio.charset.StandardCharsets
+import javax.annotation.PostConstruct
+
+@Configuration
+class FcmConfig(
+    @Value("\${firebase.path}")
+    private val fcmValue: String
+) {
+
+    @PostConstruct
+    private fun initialize() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                val options = FirebaseOptions.builder()
+                    .setCredentials(
+                        GoogleCredentials.fromStream(
+                            ByteArrayInputStream(
+                                fcmValue.toByteArray(StandardCharsets.UTF_8)
+                            )
+                        )
+                    )
+                    .build()
+                FirebaseApp.initializeApp(options)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+}

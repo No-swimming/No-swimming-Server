670420a8e96b0a7ef5a6033c4bac6f6ebb54c6ff
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-20 21:18:22 +0900

    ⚡️ :: MyBook 추가

---
+++ src/main/kotlin/com/example/noswimmingserver/domain/book/domain/MyBook.kt
@@ -0,0 +1,20 @@
+package com.example.noswimmingserver.domain.book.domain
+
+import com.example.noswimmingserver.domain.common_user.domain.User
+import javax.persistence.*
+
+@Entity
+@Table(name = "tbl_my_book")
+class MyBook(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id", nullable = false)
+    val book: Book,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+)

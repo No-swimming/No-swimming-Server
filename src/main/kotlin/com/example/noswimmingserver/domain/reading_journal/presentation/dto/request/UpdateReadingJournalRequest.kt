d9fe9173d64edada2ebec631b8f480d1fabab6c1
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-29 19:21:54 +0900

    ⚡️ :: request 그리고 service에 teacherId 추가

--- src/main/kotlin/com/example/noswimmingserver/domain/reading_journal/presentation/dto/request/UpdateReadingJournalRequest.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/reading_journal/presentation/dto/request/UpdateReadingJournalRequest.kt
@@ -1,6 +1,7 @@
 package com.example.noswimmingserver.domain.reading_journal.presentation.dto.request
 
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 import javax.validation.constraints.Size
 
 data class UpdateReadingJournalRequest(
@@ -12,4 +13,7 @@ data class UpdateReadingJournalRequest(
     @field:NotBlank
     @Size(max = 5000)
     val content: String,
+
+    @field:NotNull
+    val teacherId: Long,
 )
--- src/main/kotlin/com/example/noswimmingserver/domain/reading_journal/service/UpdateReadingJournalService.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/reading_journal/service/UpdateReadingJournalService.kt
@@ -3,6 +3,7 @@ package com.example.noswimmingserver.domain.reading_journal.service
 import com.example.noswimmingserver.domain.reading_journal.exception.CannotUpdateReadingJournalException
 import com.example.noswimmingserver.domain.reading_journal.facade.ReadingJournalFacade
 import com.example.noswimmingserver.domain.reading_journal.presentation.dto.request.UpdateReadingJournalRequest
+import com.example.noswimmingserver.domain.teacher.facade.TeacherFacade
 import com.example.noswimmingserver.global.security.SecurityFacade
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,11 +12,14 @@ import org.springframework.transaction.annotation.Transactional
 class UpdateReadingJournalService(
     private val securityFacade: SecurityFacade,
     private val readingJournalFacade: ReadingJournalFacade,
+    private val teacherFacade: TeacherFacade,
 ) {
 
     @Transactional
     fun execute(readingJournalId: Long, request: UpdateReadingJournalRequest) {
         val user = securityFacade.getCurrentUser()
+
+        val teacher = teacherFacade.getTeacherById(request.teacherId)
 
         val readingJournal = readingJournalFacade.getReadingJournalById(readingJournalId)
 
@@ -26,6 +30,7 @@ class UpdateReadingJournalService(
         readingJournal.editReadingJournal(
             title = request.title,
             content = request.content,
+            teacher = teacher,
         )
     }
 }

e22b8d30c1e1bad4a4beb07db1cc6cd43654639d
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-01-30 15:57:08 +0900

    📑 :: 피드백 수정, 삭제 api

--- src/main/kotlin/com/example/noswimmingserver/domain/feedback/presentation/FeedbackController.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/feedback/presentation/FeedbackController.kt
@@ -1,7 +1,8 @@
 package com.example.noswimmingserver.domain.feedback.presentation
 
 import com.example.noswimmingserver.domain.feedback.presentation.dto.request.CreateFeedbackRequest
-import com.example.noswimmingserver.domain.feedback.service.CreateFeedbackService
+import com.example.noswimmingserver.domain.feedback.presentation.dto.request.UpdateFeedbackRequest
+import com.example.noswimmingserver.domain.feedback.service.FeedbackService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -9,15 +10,32 @@ import javax.validation.Valid
 @RequestMapping("/feedback")
 @RestController
 class FeedbackController(
-    private val createFeedbackService: CreateFeedbackService,
+    private val feedbackService: FeedbackService,
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{reading-journal-id}")
     fun createFeedback(
         @PathVariable("reading-journal-id") readingJournalId: Long,
-        @RequestBody @Valid request: CreateFeedbackRequest
+        @RequestBody @Valid request: CreateFeedbackRequest,
     ) {
-        createFeedbackService.execute(readingJournalId, request)
+        feedbackService.execute(readingJournalId, request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{feedback-id}")
+    fun editFeedback(
+        @PathVariable("feedback-id") feedbackId: Long,
+        @RequestBody @Valid request: UpdateFeedbackRequest,
+    ) {
+        feedbackService.execute(feedbackId, request)
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{feedback-id}")
+    fun deleteFeedback(
+        @PathVariable("feedback-id") feedbackId: Long,
+    ) {
+        feedbackService.execute(feedbackId)
     }
 }

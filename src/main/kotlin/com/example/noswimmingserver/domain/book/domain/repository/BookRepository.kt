6637cd125d9c26928a29534a880d9f952cb4e7cb
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-10 13:57:56 +0900

    ⚡️ :: BookRepository -> CustomRepository 상속 추가

--- src/main/kotlin/com/example/noswimmingserver/domain/book/domain/repository/BookRepository.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/book/domain/repository/BookRepository.kt
@@ -3,6 +3,4 @@ package com.example.noswimmingserver.domain.book.domain.repository
 import com.example.noswimmingserver.domain.book.domain.Book
 import org.springframework.data.repository.CrudRepository
 
-interface BookRepository : CrudRepository<Book, Long> {
-
-}
+interface BookRepository : CrudRepository<Book, Long>, CustomBookRepository

30b8f33b96d2c881ae934147d77ea3dbcb9a8fab
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-02-10 13:59:28 +0900

    📑 :: QueryMyBookStoreList

---
+++ src/main/kotlin/com/example/noswimmingserver/domain/book/presentation/dto/response/QueryMyBookStoreList.kt
@@ -0,0 +1,12 @@
+package com.example.noswimmingserver.domain.book.presentation.dto.response
+
+data class QueryMyBookStoreList(
+    val bookList: List<BookStoreElement>
+) {
+    data class BookStoreElement(
+        val bookId: Long,
+        val bookNum: Int,
+        val isRead: Boolean,
+        val isLiked: Boolean,
+    )
+}

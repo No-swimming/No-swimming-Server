a42bf636e5ce1e16f1a2d4067d6d5821641719e6
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-02 19:28:09 +0900

    ⚡️ :: QueryUserRankingElment -> name에 널 허용 추가

--- src/main/kotlin/com/example/noswimmingserver/domain/rank/presentation/dto/response/QueryUserRankingElement.kt
+++ src/main/kotlin/com/example/noswimmingserver/domain/rank/presentation/dto/response/QueryUserRankingElement.kt
@@ -1,7 +1,7 @@
 package com.example.noswimmingserver.domain.rank.presentation.dto.response
 
 data class QueryUserRankingElement(
+    val name: String?,
     val grade: Int,
-    val name: String,
     val journalCount: Int,
 )

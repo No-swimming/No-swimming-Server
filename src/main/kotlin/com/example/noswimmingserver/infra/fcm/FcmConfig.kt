6bad1b547ae3b016b0c60fd2ee615001d352ba37
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-03-16 16:53:03 +0900

    ♻️ :: FcmConfig

--- src/main/kotlin/com/example/noswimmingserver/infra/fcm/FcmConfig.kt
+++ src/main/kotlin/com/example/noswimmingserver/infra/fcm/FcmConfig.kt
@@ -4,9 +4,10 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
+import java.io.ByteArrayInputStream
 import java.io.IOException
+import java.nio.charset.StandardCharsets
 import javax.annotation.PostConstruct
 
 @Component
@@ -16,16 +17,18 @@ class FcmConfig(
 ) {
 
     @PostConstruct
-    fun initialize() {
+    private fun initialize() {
         try {
             if (FirebaseApp.getApps().isEmpty()) {
-                val options = FirebaseOptions.builder()
+                val options: FirebaseOptions = FirebaseOptions.builder()
                     .setCredentials(
-                        GoogleCredentials.fromStream
-                            (ClassPathResource(fcmValue).inputStream)
+                        GoogleCredentials.fromStream(
+                            ByteArrayInputStream(
+                                fcmValue.toByteArray(StandardCharsets.UTF_8)
+                            )
+                        )
                     )
                     .build()
-
                 FirebaseApp.initializeApp(options)
             }
         } catch (e: IOException) {
